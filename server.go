e82db06506727ef1bc50f55cb3b6893b400f1274
Muhammad Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2024-01-17 19:03:22 +0500

    Update server.go

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
+// Import Packages and Libraries 
 import (
 	"fmt"
 	"net/http"
 	"html/template"
 )
 
+//Func for HTML template Rendering
 func login(w http.ResponseWriter, r *http.Request) {
   var filename ="login1.html"
   t, err :=template.ParseFiles(filename)
